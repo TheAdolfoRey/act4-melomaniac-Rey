94eb34d1a0afe614bf6ccb38b0803db8b4636ebc
ReyAdolfo <adolfomontero3rey@gmail.com>
2022-04-21 09:11:53 +0800

    initial file

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MUSIX </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 1.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 2.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song 3.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 4.php">Song 5</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 5.php">Song 6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
+                <p class="lead">This page provides the top 6 music of mine.</p>
+
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Pahinga</h2>
+                        <p class="lead">Al James</p>
+                        <audio id = "s1" controls> <source src="music/Pahinga.mp3" type="audio/mp3"> </audio>
+
+                        <?php
+                        echo "<br/>";
+                        echo "<br/>";
+                        echo "<p> Pahinga ka muna pahinga ka muna </p>";
+                        echo "<p> At dadalhin kita sa may alapaap </p>";
+                        echo "<p> Mga pasakit sa mundo napapagod ang puso </p>";
+                        echo "<p> Alak o usok akong bahala sayo </p>";
+                        echo "<br/>";
+                        echo "<p> Pahinga ka muna pahinga ka muna </p>";
+                        echo "<p> At dadalhin kita sa may alapaap </p>";
+                        echo "<p> Mga pasakit sa mundo napapagod ang puso </p>";
+                        echo "<p> Alak o usok akong bahala sayo </p>";
+                        echo "<br/>";
+                        echo "<p> Pagod ka na ba at sawa ka nang masaktan </p>";
+                        echo "<p> Sagad na sagad halikat' sindihan ko na yan </p>";
+                        echo "<p> Manood tayo hangang umaga ng pelikula na gusto </p>";
+                        echo "<p> Humihigpit bigla ang pagkapit mo paglilitaw ang multo </p>";
+                        echo "<br/>";
+                        echo "<p> Teka lang di ka ba hinahanap sa inyo </p>";
+                        echo "<p> Pagkasapit na ng madaling araw at wala ka </p>";
+                        echo "<p> Sabi mo hindi naman kasi napasarap narin naman </p>";
+                        echo "<p> Ang paghiga mo dito saking kama </p>";
+                        echo "<br/>";
+                        echo "<p> Nako yan ang sinasabi ko </p>";
+                        echo "<p> Di na tinapos at ayaw na niyang manood </p>";
+                        echo "<br/>";
+                        echo "<p> Pahinga ka muna pahinga ka muna </p>";
+                        echo "<p> At dadalhin kita sa may alapaap </p>";
+                        echo "<p> Mga pasakit sa mundo napapagod ang puso </p>";
+                        echo "<p> Alak o usok akong bahala sayo </p>";
+                        echo "<br/>";
+                        echo "<p> Pahinga ka muna pahinga ka muna </p>";
+                        echo "<p> At dadalhin kita sa may alapaap </p>";
+                        echo "<p> Mga pasakit sa mundo napapagod ang puso </p>";
+                        echo "<p> Alak o usok akong bahala sayo </p>";
+                        echo "<br/>";
+                        echo "<p> Panahon ay malamig pa sa nakasalin sa baso </p>";
+                        echo "<p> Lamig na para sayo hindi na bago </p>";
+                        echo "<p> Ginawa mong lahat kapalit naman ay ginagago ka niya </p>";
+                        echo "<p> Halika na sumama ka sa alapaap</p>";
+                        echo "<p> Kalimutan ang pait sa mga nalasahan </p>";
+                        echo "<p> Palapit ng palapit hanggang sa makatabi </p>";
+                        echo "<p> Na natin ang mga tala doon sa kalawakan </p>";
+                        echo "<p> At mahawakan mo na </p>";
+                        echo "<br/>";
+                        echo "<p> Kumapit ka lang lilipas ang araw at buwan </p>";
+                        echo "<p> Na tayong dalawa na lang </p>";
+                        echo "<p> Wag kang kabahan kasi mga sikreto lang natin ang makakaalam </p>";
+                        echo "<p> Sa mga nakaw na sandali at pano ginawang malaya kahit merong tinatago </p>";
+                        echo "<p> Mali ba na nakangiti pa tayo di sanay ikaw ang katabi pag bangon </p>";
+                        echo "<br/>";
+                        echo "<p> Teka lang di ka ba hinahanap sa inyo </p>";
+                        echo "<p> Pagkasapit na ng madaling araw at wala ka </p>";
+                        echo "<p> Sabi mo hindi naman kasi napasarap na rin naman kwentuhan natin at ang makasama ako </p>";
+                        echo "<br/>";
+                        echo "<p> Yan ang sinasabi mo </p>";
+                        echo "<p> Dito ka lang pwede bang wag ka nang lumayo </p>";
+                        echo "<br/>";
+                        echo "<p> Pahinga ka muna pahinga ka muna </p>";
+                        echo "<p> At dadalhin kita sa may alapaap </p>";
+                        echo "<p> Mga pasakit sa mundo napapagod ang puso </p>";
+                        echo "<p> Alak o usok akong bahala sayo </p>";
+                        echo "<br/>";
+                        echo "<p> Pahinga ka muna pahinga ka muna </p>";
+                        echo "<p> At dadalhin kita sa may alapaap </p>";
+                        echo "<p> Mga pasakit sa mundo napapagod ang puso </p>";
+                        echo "<p> Alak o usok akong bahala sayo </p>";
+
+
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Adolfo III Montero Rey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
