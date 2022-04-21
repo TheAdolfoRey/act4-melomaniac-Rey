9c78cfff1965badcb7ea4de12a13790bfb1cdac7
ReyAdolfo <adolfomontero3rey@gmail.com>
2022-04-21 09:25:07 +0800

    song 2 added

---
+++ song 2.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MUSIX </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 1.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 2.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song 3.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 4.php">Song 5</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 5.php">Song 6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
+                <p class="lead">This page provides the top 6 music of mine.</p>
+
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Trouble</h2>
+                        <p class="lead">Troye Sivan, Jay Som</p>
+                        <audio id = "s1" controls> <source src="music/Trouble.mp3" type="audio/mp3"> </audio>
+
+                        <?php
+                        echo "<br/>";
+                        echo "<br/>";
+                        echo "<p> Accidental lover </p>";
+                        echo "<p> What'd you do with all my common sense? </p>";
+                        echo "<p> You came in undercover </p>";
+                        echo "<p> When I didn't even want a friend </p>";
+                        echo "<br/>";
+                        echo "<p> I'm tired of livin' like I'm dyin' </p>";
+                        echo "<p> But now I can't find the silver linin' like before </p>";
+                        echo "<p> Yeah, like before, and you've got a funny sense of timin'</p>";
+                        echo "<p> Showed up on the night that I stopped tryin' for somethin' more </p>";
+                        echo "<p> For somethin' more </p>";
+                        echo "<br/>";
+                        echo "<p> And I'm like, Oh, sweet God, I think I'm in trouble </p>";
+                        echo "<p> Kinda makin' me wonder what it's like on the other side </p>";
+                        echo "<p> And I'm like, Baby, you're kinda drivin' mе crazy </p>";
+                        echo "<p> I'm not one to need saving, but I guess I'll give it a try </p>";
+                        echo "<br/>";
+                        echo "<p> Oh my, oh my </p>";
+                        echo "<p> Tandеm lover, what I tell you is in confidence </p>";
+                        echo "<p> Layin' here together </p>";
+                        echo "<p> Makes me wanna take a second chance </p>";
+                        echo "<br/>";
+                        echo "<p> And my room is spinnin' when I'm sober</p>";
+                        echo "<p> So somebody wake me in October, oh my God </p>";
+                        echo "<p> You've got a funny sense of knowin'</p>";
+                        echo "<p> I don't know where all this is goin', but don't stop</p>";
+                        echo "<br/>";
+                        echo "<p> And I'm like, Oh, sweet God, I think I'm in trouble </p>";
+                        echo "<p> Kinda makin' me wonder what it's like on the other side </p>";
+                        echo "<p> And I'm like, Baby, you're kinda drivin' mе crazy </p>";
+                        echo "<p> I'm not one to need saving, but I guess I'll give it a try </p>";
+                        echo "<p> Oh my, oh my </p>";
+                        echo "<br/>";
+                        echo "<p> You've got me goin' under </p>";
+                        echo "<p> Can't help but wonder if you'd be there by my side </p>";
+                        echo "<p> Or should I just move along all by myself? </p>";
+                        echo "<p> You've got me goin' under </p>";
+                        echo "<p> Can't help but wonder if you'd be there by my side </p>";
+                        echo "<p> Or should I, I, I? </p>";
+                        echo "<br/>";
+                        echo "<p> But I'm like, Oh shit, God, I think we're in trouble </p>";
+                        echo "<p> Kinda makin' me wonder what it's like on the other side </p>";
+                        echo "<p> And, ooh baby, you're kinda drivin' me crazy </p>";
+                        echo "<p> I'm not one to need saving, but I guess I'll give it a try</p>";
+                        echo "<p> Oh my, oh my </p>";
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Adolfo III Montero Rey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
