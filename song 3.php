11eaa525a90a0bbd1fb41eaf3a7601a4c7bdbfae
ReyAdolfo <adolfomontero3rey@gmail.com>
2022-04-21 09:27:00 +0800

    song 3 added

---
+++ song 3.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MUSIX </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 1.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 2.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song 3.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 4.php">Song 5</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 5.php">Song 6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
+                <p class="lead">This page provides the top 6 music of mine.</p>
+
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">踊り子</h2>
+                        <p class="lead">Vaundy</p>
+                        <audio id = "s1" controls> <source src="music/踊り子.mp3" type="audio/mp3"> </audio>
+
+                        <?php
+                        echo "<br/>";
+                        echo "<br/>";
+                        echo "<p> Hey, thought all this'd be left behind us somewhere </p>";
+                        echo "<p> Though now piece by piece they're resurfacing </p>";
+                        echo "<p> Hey, let's put them back into place properly </p>";
+                        echo "<p> Before the bubble bursts and we forget it all </p>";
+                        echo "<br/>";
+                        echo "<p> Spinning round and round, my and this girl's future </p>";
+                        echo "<p> When it stops, we'll start it up again</p>";
+                        echo "<p> Spinning round and round, me and this girl are both </p>";
+                        echo "<p> Acting like victims as we march on from where we left off</p>";
+                        echo "<br/>";
+                        echo "<p> You know, I met with you </p>";
+                        echo "<p> Though that encounter was left behind in my dreams</p>";
+                        echo "<p> Hey, why is it that you like me </p>";
+                        echo "<p> When you haven't met up with me more than once? </p>";
+                        echo "<br/>";
+                        echo "<p> Stamping down my emotions </p>";
+                        echo "<p> When the two of us were alone together </p>";
+                        echo "<p> I sing of an unforgettable love </p>";
+                        echo "<p> The words issued to us both </p>";
+                        echo "<p> As we swore a vow </p>";
+                        echo "<p> Like we'd been singing a song of an unforgettable love </p>";
+                        echo "<br/>";
+                        echo "<p> Spinning round and round, my and this girl's future </p>";
+                        echo "<p> When it stops, we'll start it up again</p>";
+                        echo "<p> Spinning round and round, me and this girl are both </p>";
+                        echo "<p> Acting like victims as we march on from where we left off</p>";
+                        echo "<br/>";
+                        echo "<p> Spinning round and round, my and this girl's future </p>";
+                        echo "<p> When it stops, we'll start it up again</p>";
+                        echo "<p> Spinning round and round, me and this girl are both </p>";
+                        echo "<p> Acting like victims as we march on from where we left off</p>";
+                        echo "<br/>";
+                        echo "<p> As we go with the times </p>";
+                        echo "<p> It's like we live in unchanging love </p>";
+                        echo "<p> When we separate, I wonder if what remains </p>";
+                        echo "<p> Will be an unchanged song of love </p>";
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Adolfo III Montero Rey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
