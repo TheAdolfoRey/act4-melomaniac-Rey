680df640f4bedae95bacd37ac3e1764cd08fc46f
ReyAdolfo <adolfomontero3rey@gmail.com>
2022-04-21 09:28:47 +0800

    song 5 added

---
+++ song 5.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MUSIX </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 1.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 2.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song 3.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 4.php">Song 5</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 5.php">Song 6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
+                <p class="lead">This page provides the top 6 music of mine.</p>
+
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">After Dark</h2>
+                        <p class="lead">Mr.Kitty</p>
+                        <audio id = "s1" controls> <source src="music/After Dark.mp3" type="audio/mp3"> </audio>
+
+                        <?php
+                        echo "<br/>";
+                        echo "<br/>";
+                        echo "<p> I see you </p>";
+                        echo "<p> You see me </p>";
+                        echo "<p> How pleasant </p>";
+                        echo "<p> This feeling </p>";
+                        echo "<p> The moment </p>";
+                        echo "<p> You hold me </p>";
+                        echo "<p> I missed you </p>";
+                        echo "<p> I'm sorry </p>";
+                        echo "<p> I've given </p>";
+                        echo "<p> What I have </p>";
+                        echo "<p> I showed you </p>";
+                        echo "<p> I'm growing </p>";
+                        echo "<p> The ashes </p>";
+                        echo "<p> Fall slowly </p>";
+                        echo "<p> As your voice  </p>";
+                        echo "<p> Consoles me </p>";
+                        echo "<br/>";
+                        echo "<p> As the hours pass </p>";
+                        echo "<p> I will let you know </p>";
+                        echo "<p> That I need to ask </p>";
+                        echo "<p> Before I'm alone </p>";
+                        echo "<p> How it feels to rest</p>";
+                        echo "<p> On your patient lips</p>";
+                        echo "<p> To eternal bliss</p>";
+                        echo "<p> I'm so glad to know </p>";
+                        echo "<br/>";
+                        echo "<p> As the hours pass </p>";
+                        echo "<p> I will let you know </p>";
+                        echo "<p> That I need to ask </p>";
+                        echo "<p> Before I'm alone </p>";
+                        echo "<p> How it feels to rest</p>";
+                        echo "<p> On your patient lips</p>";
+                        echo "<p> To eternal bliss</p>";
+                        echo "<p> I'm so glad to know </p>";
+                        echo "<br/>";
+                        echo "<p> We're swaying </p>";
+                        echo "<p> To drum beats </p>";
+                        echo "<p> In motion </p>";
+                        echo "<p> I'm feeling </p>";
+                        echo "<p> My patience</p>";
+                        echo "<p> The question</p>";
+                        echo "<p> I won't speak</p>";
+                        echo "<p> We're telling </p>";
+                        echo "<p> The stories </p>";
+                        echo "<p> Our laughter </p>";
+                        echo "<p> He knows me </p>";
+                        echo "<p> We're leaving </p>";
+                        echo "<p> We're talking</p>";
+                        echo "<p> You're closer </p>";
+                        echo "<p> It's calming</p>";
+                        echo "<br/>";
+                        echo "<p> As the hours pass </p>";
+                        echo "<p> I will let you know </p>";
+                        echo "<p> That I need to ask </p>";
+                        echo "<p> Before I'm alone </p>";
+                        echo "<p> How it feels to rest</p>";
+                        echo "<p> On your patient lips</p>";
+                        echo "<p> To eternal bliss</p>";
+                        echo "<p> I'm so glad to know </p>";
+                        echo "<br/>";
+                        echo "<p> The night will hold us close </p>";
+                        echo "<p> And the stars will guide us home </p>";
+                        echo "<p> I've been waiting for this moment </p>";
+                        echo "<p> We're finally alone </p>";
+                        echo "<p> I turn to ask the question</p>";
+                        echo "<p> So anxious, my thoughts</p>";
+                        echo "<p> Your lips were soft like winter</p>";
+                        echo "<p> In your passion, I was lost </p>";
+                        echo "<br/>";
+                        echo "<p> As the hours pass </p>";
+                        echo "<p> I will let you know </p>";
+                        echo "<p> That I need to ask </p>";
+                        echo "<p> Before I'm alone </p>";
+                        echo "<p> How it feels to rest</p>";
+                        echo "<p> On your patient lips</p>";
+                        echo "<p> To eternal bliss</p>";
+                        echo "<p> I'm so glad to know </p>";
+                        echo "<br/>";
+                        echo "<p> As the hours pass </p>";
+                        echo "<p> I will let you know </p>";
+                        echo "<p> That I need to ask </p>";
+                        echo "<p> Before I'm alone </p>";
+                        echo "<p> How it feels to rest</p>";
+                        echo "<p> On your patient lips</p>";
+                        echo "<p> To eternal bliss</p>";
+                        echo "<p> I'm so glad to know </p>";
+                        echo "<br/>";
+                        echo "<p> As the hours pass </p>";
+                        echo "<p> I will let you know </p>";
+                        echo "<p> That I need to ask </p>";
+                        echo "<p> Before I'm alone </p>";
+                        echo "<p> How it feels to rest</p>";
+                        echo "<p> On your patient lips</p>";
+                        echo "<p> To eternal bliss</p>";
+                        echo "<p> I'm so glad to know </p>";
+                        echo "<br/>";
+
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Adolfo III Montero Rey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
