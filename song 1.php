7ffbbdaf029a9ec7c264bb0440abdcd373598fe7
ReyAdolfo <adolfomontero3rey@gmail.com>
2022-04-21 09:21:32 +0800

    song 1 added

---
+++ song 1.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MUSIX </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 1.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song 2.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song 3.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 4.php">Song 5</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song 5.php">Song 6</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
+                <p class="lead">This page provides the top 6 music of mine.</p>
+
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="text-primary">Urs</h2>
+                        <p class="lead">Niki</p>
+                        <audio id = "s2" controls> <source src="music/urs.mp3" type="audio/mp3"> </audio>
+
+                        <?php
+                        echo "<br/>";
+                        echo "<br/>";
+                        echo "<p> Slick wit lip lick and we sleep in our skin </p>";
+                        echo "<p> Fast as I give in (right into you)</p>";
+                        echo "<p> Too deep too fast too </p>";
+                        echo "<p> Now I can't move 'til you call and tell me to </p>";
+                        echo "<br/>";
+                        echo "<p> Now it's all guessing games I'm guessing every gaze </p>";
+                        echo "<p> Like do you don't you would you even stay if you did </p>";
+                        echo "<p> All I know is suddenly without you the bed feels too big </p>";
+                        echo "<br/>";
+                        echo "<p> Tryna find where your head is but I'm losing myself in the process </p>";
+                        echo "<p> You're bad for my health all the good nights </p>";
+                        echo "<p> Are they hidden goodbyes in disguise I never know </p>";
+                        echo "<p> You come see me only when I ask first </p>";
+                        echo "<p> When you kiss me do you wish it were her </p>";
+                        echo "<p> The best at being the worst </p>";
+                        echo "<p> But fuck sake I'm already yours mh </p>";
+                        echo "<p> Sad to say I'm already yours oh </p>";
+                        echo "<br/>";
+                        echo "<p> Half-past midnight your half-assed texts make sure </p>";
+                        echo "<p> I keep crawlin' back for more </p>";
+                        echo "<p> Know you didn't but I'm hoping you lost your phone </p>";
+                        echo "<p> Probably with her but I'll act like I didn’t know </p>";
+                        echo "<p> I can't let it show I want nothing more than to call you up </p>";
+                        echo "<br/>";
+                        echo "<p> So instead I'll just call your bluff like </p>";
+                        echo "<p> Fuck this charade enough is enough but no </p>";
+                        echo "<p> Stick around even if it's just for the temporary show </p>";
+                        echo "<br/>";
+                        echo "<p> Tryna find where your head is but I'm losing myself in the process </p>";
+                        echo "<p> You're bad for my health all the good nights </p>";
+                        echo "<p> Are they hidden goodbyes in disguise I never know </p>";
+                        echo "<p> You come see me only when I ask first </p>";
+                        echo "<p> When you kiss me do you wish it were her </p>";
+                        echo "<p> The best at being the worst </p>";
+                        echo "<p> But fuck sake I'm already yours mh </p>";
+                        echo "<p> Sad to say I'm already yours oh </p>";
+
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Adolfo III Montero Rey</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
